d0cad9f1b55258c177f5bd1dea2408132f50d982
Edward Singleton <Edwardtonnn@gmail.com>
2023-04-17 23:29:41 -0700

    Includes Bootstrap and GSAP - working fade in example using GSAP fromTo

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react'
+import {gsap} from 'gsap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const listItems = useRef([]);
+
+  useEffect(() => {
+    gsap.fromTo(listItems.current,{
+      opacity:0
+    }, {
+      opacity: 1,
+      duration: 1,
+      stagger: 0.2
+    });
+  }, []);
+  
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+         <ul>
+      <li ref={el => listItems.current[0] = el}>Item 1</li>
+      <li ref={el => listItems.current[1] = el}>Item 2</li>
+      <li ref={el => listItems.current[2] = el}>Item 3</li>
+      <li ref={el => listItems.current[3] = el}>Item 4</li>
+      <li ref={el => listItems.current[4] = el}>Item 5</li>
+    </ul>
+        </div>
+        <div className="col-6">Testing</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
